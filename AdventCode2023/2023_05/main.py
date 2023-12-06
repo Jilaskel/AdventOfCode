6d112748b196e2681f997ac8eea3fa109e522e9f
Thomas Fabbri <thomas.fabbri@corys.fr>
2023-12-06 01:54:35 +0100

    2023_05

--- AdventCode2023/2023_05/main.py
+++ AdventCode2023/2023_05/main.py
@@ -1,7 +1,8 @@
+import queue
 prefix = "AdventCode2023/2023_05/" 
 
-fileName = "test.txt"
-# fileName = "input.txt"
+# fileName = "test.txt"
+fileName = "input.txt"
 
 fileName = prefix + fileName 
 
@@ -57,66 +58,66 @@ fileName = prefix + fileName
 ## QUEST 2
 with open(fileName) as file:
     data = file.readlines()
+    data.append("\n")  ## for last
     ans = 0
     j = 0
     ### QUESTION 2
-    seeds = []
+    seeds = queue.Queue()
     for indA,a in enumerate(data[0].split(": ")[1].strip("\n").split()):
         if indA%2==0:
             begin = int(a)
         else:
-            seeds.append((begin,begin+int(a)))
+            seeds.put((begin,begin+int(a)))
 
-    dicList = []
     for iLine,line in enumerate(data[2:]):
         if ":" in line:
-            if dicList!=[]:
-                j += 1
-            dicList.append(dict())
+            B = []
             
         elif (line=="\n"):
-            new_s = []
-            # print(seeds)
-            for (s,sEnd) in seeds:
-                if str(s) in dicList[j]:
-                    new_s.append(dicList[j][str(s)])
-                else:
-                    new_s.append(int(s))
-                if str(sEnd) in dicList[j]:
-                    new_s.append(dicList[j][str(sEnd)])
-                else:
-                    new_s.append(int(sEnd))
+            new_s = queue.Queue()
+            while not(seeds.empty()):
+                (s,sEnd) = seeds.get()
+                found = False
+                for A in B:
+                    if A[1]<=s<=A[1]+A[2]:
+                        dh = s - A[1]
+                        h = A[0] + dh
+                        newS = h
+                        if sEnd<=A[1]+A[2]:
+                            dh = sEnd - A[1]
+                            h = A[0] + dh
+                            newSEnd = h
+                            new_s.put((newS,newSEnd))
+                        else:
+                            new_s.put((newS,A[0]+A[2]))
+                            seeds.put((A[1]+A[2]+1,sEnd))
+                        found = True
+                        break
+                    elif A[1]<=sEnd<=A[1]+A[2]:
+                        # raise Exception("Merde")
+                        dh = sEnd - A[1]
+                        h = A[0] + dh
+                        newSend = h
+                        new_s.put((A[0],newSend))
+                        seeds.put((s,A[1]-1))
+                        found = True
+                        break
+                if not found:
+                    new_s.put((s,sEnd))
+                    
             seeds = new_s
         else:
             line = line.strip("\n")
-            B = [int(b) for b in line.split()]
+            B.append([int(b) for b in line.split()])
             ind = 0
-            
-            # while newL!=[]:
-            newL = []
-            for (s,sEnd) in seeds:
-                if B[1]<=s<B[1]+B[2]:
-                    dh = s - B[1]
-                    h = B[0] + dh
-                    dicList[j][str(s)] = h
-                if sEnd<B[1]+B[2]:
-                    dh = sEnd - B[1]
-                    h = B[0] + dh
-                    dicList[j][str(sEnd)] = h
-                else:
-                    newL.append((B[1]+B[2],sEnd))
-            seeds.extend(newL)
+        
 
 
-new_s = []
+final = []
+while not(seeds.empty()):
+    s=seeds.get()
+    # print(s)
+    final.append(s[0])
 # print(seeds)
-for s in seeds:
-    if str(s) in dicList[j]:
-        new_s.append(dicList[j][str(s)])
-    else:
-        new_s.append(int(s))
-seeds = new_s
-
-# print(seeds)
-ans = min(seeds)
+ans = min(final)
 print(ans)
