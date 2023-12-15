f2d156b05e276bd997cdbcbdda3c8b458a9dad43
Jilaskel <tomfabbri@live.fr>
2023-12-15 18:52:52 +0100

    2023 09, recursive solution was also possible

--- AdventCode2023/2023_09/main.py
+++ AdventCode2023/2023_09/main.py
@@ -1,4 +1,4 @@
-prefix = "AdventCode2023/2023_08/" 
+prefix = "AdventCode2023/2023_09/" 
 
 # fileName = "test.txt"
 # fileName = "test2.txt"
@@ -8,3 +8,46 @@ fileName = prefix + fileName
 
 with open(fileName) as file:
     data = file.readlines()
+
+## QUESTION 1 
+ans = 0
+for line in data:
+    line = line.strip("\n")
+    line = line.split()
+    ansLine = int(line[-1])
+    not_finished = True
+    while (not_finished):
+        newLine = []
+        not_finished = False
+        for i in range(len(line)-1):
+            a = int(line[i+1])-int(line[i])
+            newLine.append(a)
+            if a != 0:
+                not_finished = True
+        ansLine += newLine[-1]
+        line = newLine
+    ans += ansLine
+print(ans)
+
+## QUESTION 2
+ans = 0
+for line in data:
+    line = line.strip("\n")
+    line = line.split()
+    ansLine = int(line[0])
+    coeff = 1
+    not_finished = True
+    while (not_finished):
+        newLine = []
+        not_finished = False
+        for i in range(len(line)-1):
+            a = int(line[i+1])-int(line[i])
+            newLine.append(a)
+            if a != 0:
+                not_finished = True
+        ansLine -= newLine[0]*coeff
+        coeff *= -1
+        line = newLine
+    # print(ansLine)
+    ans += ansLine
+print(ans)
